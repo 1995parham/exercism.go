fb2e0df1817be21d3ff7b8fa6c0e91c4aae3fd2d
Parham Alvani <parham.alvani@gmail.com>
2018-09-13 16:15:24 +0430

    Adds clock and luhn problems.

---
+++ luhn/luhn.go
@@ -0,0 +1,51 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 30-07-2018
+ * |
+ * | File Name:     luhn.go
+ * +===============================================
+ */
+
+package luhn
+
+import (
+	"strconv"
+	"strings"
+)
+
+// Valid validates given string with luhn algorithm
+func Valid(input string) bool {
+	input = strings.Replace(input, " ", "", -1)
+	if len(input) <= 1 {
+		return false
+	}
+
+	number, err := strconv.Atoi(input)
+	if err != nil {
+		return false
+	}
+
+	count := 0
+	sum := 0
+	for number != 0 {
+		i := number % 10
+		if count%2 == 1 {
+			i = i * 2
+			if i > 9 {
+				i -= 9
+			}
+			sum += i
+		} else {
+			sum += i
+		}
+		number /= 10
+		count++
+	}
+
+	return sum%10 == 0
+}
