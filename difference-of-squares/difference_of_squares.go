ff1e3dbc12e1ba9a45510f77cfadf30aa7933559
Parham Alvani <parham.alvani@gmail.com>
2022-06-12 10:36:05 +0430

    fix: correct difference of squares

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,37 +1,26 @@
-/*
- *
- * In The Name of God
- *
- * +===============================================
- * | Author:        Parham Alvani <parham.alvani@gmail.com>
- * |
- * | Creation Date: 28-07-2018
- * |
- * | File Name:     difference_of_squares.go
- * +===============================================
- */
-
 package diffsquares
 
-// SumOfSquares is 1 * 1 + 2 * 2 + ... + n * n
+// SumOfSquares is 1 * 1 + 2 * 2 + ... + n * n.
 func SumOfSquares(n int) int {
 	sum := 0
 	for i := 1; i <= n; i++ {
 		sum += i * i
 	}
+
 	return sum
 }
 
-// SquareOfSums is (1 + 2 + ... + n)^2
-func SquareOfSums(n int) int {
+// SquareOfSum is (1 + 2 + ... + n)^2.
+func SquareOfSum(n int) int {
 	sum := 0
 	for i := 0; i <= n; i++ {
 		sum += i
 	}
+
 	return sum * sum
 }
 
-// Difference returns difference of (1 + 2 + ... + n)^2 - (1 * 1 + 2 * 2 + ... + n * n)
+// Difference returns difference of (1 + 2 + ... + n)^2 - (1 * 1 + 2 * 2 + ... + n * n).
 func Difference(n int) int {
-	return SquareOfSums(n) - SumOfSquares(n)
+	return SquareOfSum(n) - SumOfSquares(n)
 }
