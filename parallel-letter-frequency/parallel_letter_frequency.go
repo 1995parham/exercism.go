584465623297995081e5da0dc36b0044a31125b6
Parham Alvani <parham.alvani@gmail.com>
2022-06-12 10:38:29 +0430

    fix: parallel letter frequency

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,14 +1,58 @@
 package letter
 
-// FreqMap records the frequency of each rune in a given text.
+import (
+	"runtime"
+	"sync"
+)
+
+// FreqMap is rune frequency counter
 type FreqMap map[rune]int
 
-// Frequency counts the frequency of each rune in a given text and returns this
-// data as a FreqMap.
+// Frequency calculates rune frequency in each word
 func Frequency(s string) FreqMap {
 	m := FreqMap{}
 	for _, r := range s {
 		m[r]++
 	}
 	return m
+}
+
+// ConcurrentFrequency calls frequency on each word
+func ConcurrentFrequency(ss []string) FreqMap {
+	wordStream := make(chan string)
+
+	go func() {
+		for _, s := range ss {
+			wordStream <- s
+		}
+		close(wordStream)
+	}()
+
+	var wg sync.WaitGroup
+	freqMapStream := make(chan FreqMap)
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go func() {
+			for s := range wordStream {
+				freqMapStream <- Frequency(s)
+			}
+			wg.Done()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(freqMapStream)
+	}()
+
+	result := make(FreqMap)
+
+	for fm := range freqMapStream {
+		for r, c := range fm {
+			result[r] += c
+		}
+	}
+
+	return result
 }
